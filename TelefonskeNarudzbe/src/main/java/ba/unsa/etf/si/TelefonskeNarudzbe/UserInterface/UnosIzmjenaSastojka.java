c4b5738d6b0c57b1a3848da8d9128b019c5c8c06
Emina Huskic <huskic.emina@gmail.com>
2016-05-15 23:51:20 +0200

    Update UnosIzmjenaSastojka.java

--- TelefonskeNarudzbe/src/main/java/ba/unsa/etf/si/TelefonskeNarudzbe/UserInterface/UnosIzmjenaSastojka.java
+++ TelefonskeNarudzbe/src/main/java/ba/unsa/etf/si/TelefonskeNarudzbe/UserInterface/UnosIzmjenaSastojka.java
@@ -148,7 +148,7 @@ public class UnosIzmjenaSastojka extends JFrame {
 		contentPane.setLayout(gl_contentPane);
 	}
 	public UnosIzmjenaSastojka(Sastojak s) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 402, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
