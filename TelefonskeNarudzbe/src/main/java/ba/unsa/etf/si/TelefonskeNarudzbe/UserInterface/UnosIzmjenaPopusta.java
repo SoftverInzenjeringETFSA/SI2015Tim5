91ae6ac706e894afece4b3499621c05fa5faca2d
Emina Huskic <huskic.emina@gmail.com>
2016-05-15 23:49:51 +0200

    dispose_on_close

--- TelefonskeNarudzbe/src/main/java/ba/unsa/etf/si/TelefonskeNarudzbe/UserInterface/UnosIzmjenaPopusta.java
+++ TelefonskeNarudzbe/src/main/java/ba/unsa/etf/si/TelefonskeNarudzbe/UserInterface/UnosIzmjenaPopusta.java
@@ -58,7 +58,7 @@ public class UnosIzmjenaPopusta extends JFrame {
 	 */
 	
 	public UnosIzmjenaPopusta() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 340, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
