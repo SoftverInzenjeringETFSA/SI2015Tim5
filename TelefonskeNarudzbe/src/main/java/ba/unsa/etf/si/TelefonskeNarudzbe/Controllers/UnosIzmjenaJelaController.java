42735b20cac4a0faa2764c451cc5ebeb0576240c
Emina Huskic <huskic.emina@gmail.com>
2016-05-19 22:49:49 +0200

    Jelo fix

--- TelefonskeNarudzbe/src/main/java/ba/unsa/etf/si/TelefonskeNarudzbe/Controllers/UnosIzmjenaJelaController.java
+++ TelefonskeNarudzbe/src/main/java/ba/unsa/etf/si/TelefonskeNarudzbe/Controllers/UnosIzmjenaJelaController.java
@@ -41,101 +41,147 @@ public class UnosIzmjenaJelaController {
 	}
 
 	public boolean izmjenaJela(String naziv, String opis, Double cijena, List<Sastojak> listaSastojaka,
-			List<Double> listaKolicina, int dodajNovi,sef Sef) {
+			List<Double> listaKolicina, int dodajNovi, sef Sef) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction t = session.beginTransaction();
 
 		Session session2 = HibernateUtil.getSessionFactory().openSession();
 		Transaction t2 = session2.beginTransaction();
-		int ids =-1;
-		if(dodajNovi!=-1) ids=dodajNovi;
-		try {
-			if (session.createCriteria(Jelo.class).add(Restrictions.eq("id", ids))
-					.setProjection(Projections.property("id")).uniqueResult() == null) {
-				System.out.println("Novo jelo");
-				Jelo j = new Jelo();
-				j.setNaziv(naziv);
-				j.setOpis(opis);
-				j.setCijena(cijena);
-				j.setIzbrisano(false);
-				session.beginTransaction();
-				session.save(j);
-				session.getTransaction().commit();
-				session2.createCriteria(SastojciJeloVeza.class);
-					for (int i = 0; i < listaSastojaka.size(); i++) {
-					if (listaKolicina.get(i).equals(0))
-						continue;
-					SastojciJeloVeza sjv = new SastojciJeloVeza();
-					sjv.setJelo(j);
-					sjv.setSastojak(listaSastojaka.get(i));
-					sjv.setKolicina(listaKolicina.get(i));
-					session2.beginTransaction();
-					session2.save(sjv);
-					session2.getTransaction().commit();
-
+		int ids = -1;
+		if (dodajNovi != -1)
+			ids = dodajNovi;
+		int ide = -1;
+		boolean izbrisan = false;
+		if (ids == -1) {
+			if (session.createCriteria(Jelo.class).add(Restrictions.like("naziv", naziv).ignoreCase()).list()
+					.size() != 0) {
+				Jelo postojeci = (Jelo) session.createCriteria(Jelo.class)
+						.add(Restrictions.like("naziv", naziv).ignoreCase()).list().get(0);
+				if (!postojeci.getIzbrisano()) {
+					JOptionPane.showMessageDialog(null, "Već postoji jelo sa tim nazivom.");
+					session.close();
+					return false;
 				}
-			} else {
-				System.out.println("Postoji jelo");
-				Criteria criteria = session.createCriteria(Jelo.class).add(Restrictions.like("id", ids));
-				List<Jelo> lista = criteria.list();
-				Jelo j = lista.get(0);
-				j.setNaziv(naziv);
-				j.setOpis(opis);
-				j.setCijena(cijena);
-				j.setIzbrisano(false);
-				session.update(j);
-				t.commit();
-				session.close();
-				session2.getTransaction().begin();
-				int i = -1;
-				for (Sastojak s : listaSastojaka) {
-					i++;
-					if (s.isIzbrisan())
-						continue;
-					session2.getTransaction().begin();
-					Criteria criteria2 = session2.createCriteria(SastojciJeloVeza.class);
-					List<SastojciJeloVeza> l = criteria2.list();
-					boolean postoji = false;
-					for (SastojciJeloVeza sjv : l) {
-						if (sjv.getJelo().getId() == j.getId() && sjv.getSastojak().getId() == s.getId()) // veza
-																				// postoji
-						{
-							if (listaKolicina.get(i).equals(0))
-								session2.delete(sjv);
-							else {
-								sjv.setJelo(j);
-								sjv.setSastojak(s);
-								sjv.setKolicina(listaKolicina.get(i));
-								session2.update(sjv);
-							}
-							postoji = true;
-							break;
-						}
-					}
-					if (listaKolicina.get(i).equals(0.0))
-						continue;
-					if (!postoji) {
+				ide = postojeci.getId();
+				izbrisan = true;
+			}
+		}
+			try {
+				if (session.createCriteria(Jelo.class).add(Restrictions.eq("id", ids))
+						.setProjection(Projections.property("id")).uniqueResult() == null) {
+					System.out.println("Novo jelo");
+					if (izbrisan) {
+						Criteria criteria = session.createCriteria(Jelo.class).add(Restrictions.like("id", ide));
+						List<Jelo> l = criteria.list();
+						Jelo j = l.get(0);
+						j.setIzbrisano(false);
+						j.setNaziv(naziv);
+						j.setOpis(opis);
+						j.setCijena(cijena);
+						session.beginTransaction();
+						session.update(j);
+						session.getTransaction().commit();
 						session2.createCriteria(SastojciJeloVeza.class);
+						for (int i = 0; i < listaSastojaka.size(); i++) {
+							if (listaKolicina.get(i).equals(0.0))
+								continue;
+							SastojciJeloVeza sjv = new SastojciJeloVeza();
+							sjv.setJelo(j);
+							sjv.setSastojak(listaSastojaka.get(i));
+							sjv.setKolicina(listaKolicina.get(i));
+							session2.beginTransaction();
+							session2.save(sjv);
+							session2.getTransaction().commit();
+						}
+						Sef.refreshTabeleJelo();
+						System.out.println("Jelo je izmijenjeno");
+						return true;
+					}
+					Jelo j = new Jelo();
+
+					j.setNaziv(naziv);
+					j.setOpis(opis);
+					j.setCijena(cijena);
+					j.setIzbrisano(false);
+					session.beginTransaction();
+					session.save(j);
+					session.getTransaction().commit();
+					session2.createCriteria(SastojciJeloVeza.class);
+					for (int i = 0; i < listaSastojaka.size(); i++) {
+						if (listaKolicina.get(i).equals(0))
+							continue;
 						SastojciJeloVeza sjv = new SastojciJeloVeza();
 						sjv.setJelo(j);
-						sjv.setSastojak(s);
+						sjv.setSastojak(listaSastojaka.get(i));
 						sjv.setKolicina(listaKolicina.get(i));
+						session2.beginTransaction();
 						session2.save(sjv);
 						session2.getTransaction().commit();
 					}
-					
+
+				} else {
+					System.out.println("Postoji jelo");
+					Criteria criteria = session.createCriteria(Jelo.class).add(Restrictions.like("id", ids));
+					List<Jelo> lista = criteria.list();
+					Jelo j = lista.get(0);
+					j.setNaziv(naziv);
+					j.setOpis(opis);
+					j.setCijena(cijena);
+					j.setIzbrisano(false);
+					session.update(j);
+					t.commit();
+					session.close();
+					session2.getTransaction().begin();
+					int i = -1;
+					for (Sastojak s : listaSastojaka) {
+						i++;
+						if (s.isIzbrisan())
+							continue;
+						session2.getTransaction().begin();
+						Criteria criteria2 = session2.createCriteria(SastojciJeloVeza.class);
+						List<SastojciJeloVeza> l = criteria2.list();
+						boolean postoji = false;
+						for (SastojciJeloVeza sjv : l) {
+							if (sjv.getJelo().getId() == j.getId() && sjv.getSastojak().getId() == s.getId()) // veza
+							// postoji
+							{
+								if (listaKolicina.get(i).equals(0.0))
+									session2.delete(sjv);
+								else {
+									sjv.setJelo(j);
+									sjv.setSastojak(s);
+									sjv.setKolicina(listaKolicina.get(i));
+									session2.update(sjv);
+								}
+								postoji = true;
+								break;
+							}
+						}
+						if (listaKolicina.get(i).equals(0.0))
+							continue;
+						if (!postoji) {
+							session2.createCriteria(SastojciJeloVeza.class);
+							SastojciJeloVeza sjv = new SastojciJeloVeza();
+							sjv.setJelo(j);
+							sjv.setSastojak(s);
+							sjv.setKolicina(listaKolicina.get(i));
+							session2.save(sjv);
+							session2.getTransaction().commit();
+						}
+
+					}
+					session2.close();
+					System.out.println("Jelo je izmijenjeno");
+					return true;
 				}
-				session2.close();
-				System.out.println("Jelo je izmijenjeno");
-				return true;
+				Sef.refreshTabeleJelo();
+
+			} catch (Exception e) {
+				logger.info(e);
+				return false;
 			}
-			Sef.refreshTabeleJelo();
+			return true;
 
-		} catch (Exception e) {
-			logger.info(e);
-			return false;
 		}
-		return true;
-
 	}
-}
+
