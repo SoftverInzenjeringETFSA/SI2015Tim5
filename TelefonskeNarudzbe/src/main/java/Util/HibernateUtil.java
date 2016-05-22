0d61033549da209ad63c56eeee8a5d2a09b8edaa
MerisaGolic <mgolic1@etf.unsa.ba>
2016-05-22 19:47:37 +0200

    .jar file za instalaciju

--- TelefonskeNarudzbe/src/main/java/Util/HibernateUtil.java
+++ TelefonskeNarudzbe/src/main/java/Util/HibernateUtil.java
@@ -1,27 +1,38 @@
 package Util;
-import org.apache.log4j.Logger; 
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.imageio.spi.ServiceRegistry;
+
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	 private static final SessionFactory sessionFactory = buildSessionFactory();
-	 final static Logger logger = Logger.getLogger(HibernateUtil.class);
 
-	    private static SessionFactory buildSessionFactory() {
-	        try {
-	            // Create the SessionFactory from hibernate.cfg.xml
-	            return new Configuration().configure().buildSessionFactory();
-	            //new StandardServiceRegistryBuilder().build()
-	        }
-	        catch (Exception ex) {
-	            // Make sure you log the exception, as it might be swallowed
-	        	//logger.info(ex);
-	            System.err.println("Initial SessionFactory creation failed." + ex);
-	            throw new ExceptionInInitializerError(ex);
-	        }
-	    }
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+	final static Logger logger = Logger.getLogger(HibernateUtil.class);
 
-	    public static SessionFactory getSessionFactory() {
-	        return sessionFactory;
-	    }
+	private static SessionFactory buildSessionFactory() {
+		try {
+			java.util.Properties properties = new Properties();
+			properties.load(new FileInputStream("db.properties"));
+			// Create the SessionFactory from hibernate.cfg.xml
+			return new Configuration().addProperties(properties).configure().buildSessionFactory();
+		} catch (Exception ex) {
+			// Make sure you log the exception, as it might be swallowed
+			//System.err.println("Initial SessionFactory creation failed." + ex);
+			logger.info(ex);
+			throw new ExceptionInInitializerError(ex);	
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+
+	}
+
 }
